7769173ffbda6660898c4a231b82ec947ecfbc36
umar muhaimin <umarmuhaiminbusiness@gmail.com>
2025-09-11 12:32:47 +0800

    update

--- Problem Set 0/task_4.py
+++ Problem Set 0/task_4.py
@@ -18,3 +18,16 @@ main()
 
 # REMEMBER, everytime you cross a situation / task that requires square, cubic or to the power of n ; you need to you the square/cubic/power function.
 # In this case, always remember to define either variable function or numerical {such as squared} function in order to execute it.
+
+# Second Method : Easy method
+
+def main():
+    m = int(input("m:"))
+    C = 300000000
+    E = m * C ** 2 # Note : C ** 2 means C^2
+    print(E)
+
+main()
+
+# Since CS50P wants a program in Python that prompts the user for mass, you need to prompt it like this int(input("m:")).
+# You need to include the prompt 'Mass in any of the form you comfortable with".
