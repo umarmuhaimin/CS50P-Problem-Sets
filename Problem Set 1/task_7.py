e7a8d673fcb7375c7f204db7d9a66a29124a89e1
umar muhaimin <umarmuhaiminbusiness@gmail.com>
2025-09-15 13:03:44 +0800

    update

--- Problem Set 1/task_7.py
+++ Problem Set 1/task_7.py
@@ -40,7 +40,7 @@ def greeting_incorrect(greet):
 
 main()
 
-### The reason why we cannot use return function is because return greet.strip() in correct_greet only checks the exact match / answer which is only {"hello"} ###
+### The reason why we cannot use return function "standalone" is because return greet.strip() in correct_greet only checks the exact match / answer which is only {"hello"} ###
 # It means that greet.strip() in correct_greet will only return True if the input is exactly "hello".
 
 # "hello" → ✅ True
@@ -51,7 +51,7 @@ main()
 # We can use return function only if it's a true or false scenario/case.
 # In this task, anything that starts with {hello} whether it is capitilize or small caps : it will be acceptable, same goes to anything that starts with "h" whether it is capitilize or small caps since we have .lower() function.
 
-### That is why we need to use .startswith() function so that nything that starts with "hello" or "h" will be acceptable and otherwise false ###
+### That is why we need to use return .startswith() function so that anything that starts with "hello" or "h" will be acceptable and otherwise false ###
 # The function startswith("hello") allows:
 
 # "hello" ✅
@@ -59,6 +59,6 @@ main()
 # "Hello" ✅ (if you first .lower() the string)
 
 ### Conclusion ###
-# 1. Use return function only if the answer in the case/task are fixed answers/data. return function is used for true or false scenario/case. return function is used in strict case (it can only acccept fixed answers/data only).
-# 2. Use .startswith() function if the correct answer for the case/task begins with a certain letters or words (e.g., "hello" or "hello there" or "h"), the answers will be acceptable instead of requiring the entire answer to be an exact match. If it requires to be an exact match, then use return function.
-# 3. .startswith() function is more lenient.
+# 1. Use return function "standalone" only if the answer in the case/task are fixed answers/data. return function is used for true or false scenario/case. return function is used in strict case (it can only acccept fixed answers/data only).
+# 2. Use return .startswith() function if the correct answer for the case/task begins with a certain letters or words (e.g., "hello" or "hello there" or "h"), the answers will be acceptable instead of requiring the entire answer to be an exact match. If it requires to be an exact match, then use return function.
+# 3. return .startswith() function is more lenient. It basically means return true if the input anwsers starts with a certain letter or words and accept it.
